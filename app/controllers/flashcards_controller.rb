eb90b79254489698256e13704a999c20cd07f8b6
ruby _v <lien.jennifer@gmail.com>
2013-11-10 02:49:03 -0600

    work in progress

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -3,9 +3,13 @@ require 'json'
 
 class FlashcardsController < ApplicationController
   def index
-    # url = ?
-    # raw_data = ?
+    url = "http://kiei925.herokuapp.com/students.json?access_token=broncos"
+    raw_data = open(url).read
+    @parsed_data = JSON.parse(raw_data)
 
-    # @students = ?
-  end
+	@first_name = @parsed_data[0]["first_name"]
+    @last_name = @parsed_data[0]["last_name"]
+    @photo= @parsed_data[0]["image_url"]
+
+end
 end
